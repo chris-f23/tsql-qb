2bf887caed0c9278f7344a5bcf6b8ce57d9c1c93
Christian Farias <christian.farias.aguila@gmail.com>
2025-05-27 15:35:54 -0400

    Refactoriza create-table.test.js

--- src/create-table.test.js
+++ src/create-table.test.js
@@ -2,15 +2,18 @@ import { describe, it, expect } from "@jest/globals";
 import { createTable } from "./create-table";
 import { DataType } from "./data-types";
 
+function crearTablaPersona() {
+  return createTable("Persona", {
+    Id: DataType.INTEGER(),
+    Nombre: DataType.VARCHAR(20),
+    ApellidoPaterno: DataType.VARCHAR(50),
+    ApellidoMaterno: DataType.VARCHAR(50),
+    RUT: DataType.VARCHAR(10),
+  });
+}
 describe("Módulo create-table", () => {
   it("Debe crear la tabla Persona, con sus columnas siendo un id, nombre, apellido paterno, apellido materno y rut.", () => {
-    const statement = createTable("Persona", {
-      Id: DataType.INTEGER(),
-      Nombre: DataType.VARCHAR(20),
-      ApellidoPaterno: DataType.VARCHAR(50),
-      ApellidoMaterno: DataType.VARCHAR(50),
-      RUT: DataType.VARCHAR(10),
-    }).build();
+    const statement = crearTablaPersona().build();
 
     expect(statement).toBe(
       "CREATE TABLE [Persona] (" +
@@ -24,13 +27,7 @@ describe("Módulo create-table", () => {
   });
 
   it("Debe crear la tabla Persona y definir la columna id como PRIMARY KEY.", () => {
-    const statement = createTable("Persona", {
-      Id: DataType.INTEGER(),
-      Nombre: DataType.VARCHAR(20),
-      ApellidoPaterno: DataType.VARCHAR(50),
-      ApellidoMaterno: DataType.VARCHAR(50),
-      RUT: DataType.VARCHAR(10),
-    })
+    const statement = crearTablaPersona()
       .addPrimaryKeyConstraint("PK_Persona", ["Id"])
       .build();
 
@@ -47,13 +44,7 @@ describe("Módulo create-table", () => {
   });
 
   it("Debe crear la tabla Persona y definir una restricción UNIQUE sobre la columna RUT.", () => {
-    const statement = createTable("Persona", {
-      Id: DataType.INTEGER(),
-      Nombre: DataType.VARCHAR(20),
-      ApellidoPaterno: DataType.VARCHAR(50),
-      ApellidoMaterno: DataType.VARCHAR(50),
-      RUT: DataType.VARCHAR(10),
-    })
+    const statement = crearTablaPersona()
       .addUniqueConstraint("UQ_Persona_RUT", ["RUT"])
       .build();
 
@@ -70,13 +61,7 @@ describe("Módulo create-table", () => {
   });
 
   it("Debe crear la tabla Persona y definir una restricción UNIQUE sobre las columnas nombre y apellido.", () => {
-    const statement = createTable("Persona", {
-      Id: DataType.INTEGER(),
-      Nombre: DataType.VARCHAR(20),
-      ApellidoPaterno: DataType.VARCHAR(50),
-      ApellidoMaterno: DataType.VARCHAR(50),
-      RUT: DataType.VARCHAR(10),
-    })
+    const statement = crearTablaPersona()
       .addUniqueConstraint("UQ_Persona_RUT_Nombre", ["RUT", "Nombre"])
       .build();
 
